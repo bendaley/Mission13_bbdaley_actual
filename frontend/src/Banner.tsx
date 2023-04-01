a27df5870c14ce8cb4afbfec23e2bf93e109353d
bendaley <bbdaley21@gmail.com>
2023-04-01 08:26:32 -0600

    Styling complete

--- frontend/src/Banner.tsx
+++ frontend/src/Banner.tsx
@@ -1,24 +1,26 @@
 import React from 'react';
-// import styles from './/Home.module.css';
 
 function TopBanner(props: any) {
-  // function TopBanner({ saying }: any) {
   return (
-    // <React.Fragment>
     <>
-      <div className="row text-center">
+      <div className="text-center">
+        {' '}
+        {/* add text-center class here */}
         <h1>{props.saying}</h1>
         <br></br>
-        <div className="col-3 mx-auto">
-          {/* <img src="./logo192.png" alt="companyLogo" /> */}
-          <img src="./JoelHiltonHeadshot.jpg" alt="Joel" className="mx-auto" />
-        </div>
-        <div className="col align-self-center text-start">
-          {/* <h1 className={styles.h1}>{saying}</h1>*/}
+        <div className="row justify-content-center text-center">
+          <div className="col-12 col-md-3">
+            <div className="border border-primary p-3">
+              <img
+                src="./JoelHiltonHeadshot.jpg"
+                alt="Joel"
+                className="img-fluid"
+              />
+            </div>
+          </div>
         </div>
       </div>
     </>
-    // </React.Fragment>
   );
 }
 
